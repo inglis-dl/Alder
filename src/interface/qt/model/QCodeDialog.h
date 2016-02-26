d7a91dd8ad5bb16eb1a26b96349304330072d27a
Dean Inglis <inglisd@mcmaster.ca>
2016-02-26 17:09:45 -0500

    WIP: Qt pimpl

--- src/interface/qt/model/QCodeDialog.h
+++ src/interface/qt/model/QCodeDialog.h
@@ -8,61 +8,38 @@
   Author: Dean Inglis <inglisd AT mcmaster DOT ca>
 
 =========================================================================*/
-
 #ifndef __QCodeDialog_h
 #define __QCodeDialog_h
 
+// Qt includes
 #include <QDialog>
-#include <map>
 
-class QTableWidgetItem;
-class Ui_QCodeDialog;
+class QCodeDialogPrivate;
 
 class QCodeDialog : public QDialog
 {
   Q_OBJECT
 
 public:
+  typedef QDialog Superclass;
   //constructor
-  QCodeDialog( QWidget* parent = 0 );
+  explicit QCodeDialog( QWidget* parent = 0 );
   //destructor
   ~QCodeDialog();
 
 signals:
 
 public slots:
-
-  void slotClose();
-
-  void slotHeaderClicked(int index);
-  void slotTableDoubleClicked();
-  void slotTabChanged();
-
-  void slotGroupSelectionChanged();
-  void slotGroupAdd();
-  void slotGroupEdit();
-  void slotGroupRemove();
-  void slotGroupApply();
-
-  void slotCodeSelectionChanged();
-  void slotCodeAdd();
-  void slotCodeEdit();
-  void slotCodeRemove();
-  void slotCodeApply();
+  void close();
 
 protected:
-  void updateInterface();
-  void updateGroupTable();
-  void updateCodeTable();
+  QScopedPointer<QCodeDialogPrivate> d_ptr;
 
 protected slots:
 
 private:
-  // Designer form
-  Ui_QCodeDialog *ui;
-
-  std::map< std::string, std::map< std::string, int > > columnIndex;
-  std::map< std::string, std::map< int, Qt::SortOrder > > sortOrder;
+  Q_DECLARE_PRIVATE(QCodeDialog);
+  Q_DISABLE_COPY(QCodeDialog);
 };
 
 #endif
---
+++ src/interface/qt/model/QCodeDialog_p.h
@@ -0,0 +1,57 @@
+/*=========================================================================
+
+  Program:  Alder (CLSA Medical Image Quality Assessment Tool)
+  Module:   QCodeDialog_p.h
+  Language: C++
+
+  Author: Dean Inglis <inglisd AT mcmaster DOT ca>
+
+=========================================================================*/
+#ifndef __QCodeDialog_p_h
+#define __QCodeDialog_p_h
+
+#include <QCodeDialog.h>
+#include <ui_QCodeDialog.h>
+
+#include <QMap>
+
+class QTableWidgetItem;
+
+class QCodeDialogPrivate : public QObject, public Ui_QCodeDialog
+{
+  Q_OBJECT
+  Q_DECLARE_PUBLIC(QCodeDialog);
+protected:
+  QCodeDialog* const q_ptr;
+
+public:
+  explicit QCodeDialogPrivate(QCodeDialog& object);
+  virtual ~QCodeDialogPrivate();
+
+  void init();
+  void setupUi(QDialog*);
+  void updateGroupUi();
+  void updateCodeUi();
+  void updateUi();
+
+public slots:
+  void sort(int);
+  void tableDoubleClicked(QTableWidgetItem*);
+  void tabChanged();
+  void selectedGroupChanged();
+  void selectedCodeChanged();
+  void addGroup();
+  void addCode();
+  void applyGroup();
+  void applyCode();
+  void removeGroup();
+  void removeCode();
+  void editGroup();
+  void editCode();
+
+private:
+  QMap<QString, QMap<QString, int>> columnIndex;
+  QMap<QString, QMap<int, Qt::SortOrder>> sortOrder;
+};
+
+#endif
